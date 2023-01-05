58533f7bf39172ec0cc94ed150e5b20e2bae9f36
EustaceCheng <fbi0258zzz@gmail.com>
2023-01-05 21:56:56 +0800

    update readme

--- README.md
+++ README.md
@@ -5,3 +5,32 @@
 ```
 yarn && yarn start
 ```
+
+### Flow
+
+1. use cra start project
+2. import craco package
+3. use craco.config.js to modify webpack
+```
+module: {
+        rules: [
+            {
+                test: /\.mp3$/,
+                loader: 'file-loader',
+                query: {
+                    name: 'static/media/[name].[hash:8].[ext]',
+                },
+            },
+        ],
+    },
+```
+4. declare mp3 type on react-app-env.d.ts 
+```
+declare module '*.mp3' {
+    const src: string
+    export default src
+}
+
+```
+
+5. easy use on any position~
--- craco.config.js
+++ craco.config.js
@@ -1,16 +1,17 @@
 module.exports = {
     // ...
     typescript: {
-      enableTypeChecking: true /* (default value) */,
-    },module: {
-      rules: [
-        {
-          test: /\.mp3$/,
-          loader: 'file-loader',
-          query: {
-            name: 'static/media/[name].[hash:8].[ext]'
-          }
-        }
-      ],
+        enableTypeChecking: true /* (default value) */,
     },
-  };
+    module: {
+        rules: [
+            {
+                test: /\.mp3$/,
+                loader: 'file-loader',
+                query: {
+                    name: 'static/media/[name].[hash:8].[ext]',
+                },
+            },
+        ],
+    },
+}
